5e2d3b82dc1656767a7d47a6e906e92027bc811c
Destio Wahyu <98136191+destiowahyu@users.noreply.github.com>
2024-07-29 10:54:27 +0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => (
     <div className="max-w-6xl mx-auto p-4 text-center text-gray-600">
       <h1 className="text-lg">PERBAIKAN NILAI UTS DESTIO</h1>
       <p className="text-sm">
-        LindungiKita
+        LindungiKita v2.1
       </p>
       <p className="text-sm mt-1">
         &copy; 2024 LindungiKita. All rights reserved.
