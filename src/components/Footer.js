ba1f052903345187b84a254806d7f88a8f2156b9
Destio Wahyu <98136191+destiowahyu@users.noreply.github.com>
2024-08-18 23:01:33 +0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Footer = () => (
   <footer className="bg-gray-200 shadow-md mt-8">
     <div className="max-w-6xl mx-auto p-4 text-center text-gray-600">
-      <h1 className="text-lg">PERBAIKAN NILAI UTS DESTIO</h1>
       <p className="text-sm">
         LindungiKita v2.1
       </p>
